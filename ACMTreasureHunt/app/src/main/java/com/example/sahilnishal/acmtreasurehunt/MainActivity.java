8159500b8e03596dd2e0c50b54464e35b839d558
snishal <snishal33@gmail.com>
2017-02-09 23:50:00 +0530

    Final App

--- ACMTreasureHunt/app/src/main/java/com/example/sahilnishal/acmtreasurehunt/MainActivity.java
+++ ACMTreasureHunt/app/src/main/java/com/example/sahilnishal/acmtreasurehunt/MainActivity.java
@@ -1,22 +1,27 @@
 package com.example.sahilnishal.acmtreasurehunt;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     EditText editText;
     Button button;
+    ImageView i;
+    final String question[] = {"question1","question2","question3","question4","question5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i = (ImageView)findViewById(R.id.imageView);
+        i.setImageResource(R.drawable.acm_logo);
         editText = (EditText)findViewById(R.id.editText);
         button = (Button)findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -25,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
                 String team_id = editText.getText().toString();
                 if(team_id.length()>0){
                     Intent i = new Intent(getApplicationContext(),Question.class);
-                    i.putExtra("team_id",team_id);
+                    i.putExtra("question",question[team_id.charAt(6)-49]);
                     startActivity(i);
                 }
                 else{
-                    Toast.makeText(getApplicationContext(),"Enter Team ID",Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Enter Team ID",Toast.LENGTH_SHORT).show();
                 }
             }
         });
--- ACMTreasureHunt/app/src/main/java/com/example/sahilnishal/acmtreasurehunt/Question.java
+++ ACMTreasureHunt/app/src/main/java/com/example/sahilnishal/acmtreasurehunt/Question.java
@@ -1,13 +1,66 @@
 package com.example.sahilnishal.acmtreasurehunt;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Question extends AppCompatActivity {
+public class Question extends Activity {
+
+    TextView textView;
+    Button button;
+    static int question = 1;
+    static int answer = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        textView = (TextView)findViewById(R.id.question);
+        textView.setText(getIntent().getExtras().getString("question"));
+        button = (Button)findViewById(R.id.scan);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Scan.class);
+                startActivityForResult(i,question);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == question){
+            if(resultCode == answer){
+                final String qr_code = data.getExtras().getString("code");
+                if(qr_code.length()>0) {
+                    final String solution = qr_code.substring(0, 3);
+                    final String next_question = qr_code.substring(3);
+                    AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                    alert.setTitle("Verify");
+                    alert.setMessage("Enter your solution");
+                    final EditText sol = new EditText(this);
+                    alert.setView(sol);
+                    alert.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // TODO Auto-generated method stub
+                            if (sol.getText().toString().equals(solution))
+                                textView.setText(next_question);
+                            else
+                                Toast.makeText(getApplicationContext(), "Wrong Answer", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    alert.show();
+                }
+            }
+        }
     }
 }
